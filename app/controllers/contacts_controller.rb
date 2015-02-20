888d1328369a093344967ede1dae22aead0fefdc
Mohamed amine bouchalkha <mabouchalkha@gmail.com>
2015-02-20 16:45:42 +0000

    contact fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,6 @@
 class ContactsController < ApplicationController
-    def create
-		@contact = Contact.new(@contact_params);
-		if @contact.save!
-			render status: :ok, json: @contact.as_json
+  def create
+	
 	end
 
 	private
